c9a89407c57385becbc61dba4c436f10caf4913f
Łukasz Andrzejewski <lukasz.andrzejewski@inbright.pl>
2017-09-06 09:30:55 +0200

    EJB

--- service/src/main/java/pl/training/bank/service/Mapper.java
+++ service/src/main/java/pl/training/bank/service/Mapper.java
@@ -10,18 +10,24 @@ import java.util.stream.Collectors;
 public class Mapper {
 
     public <Source, Destination> void map(Source source, Destination destination) {
-        new ModelMapper().map(source, destination);
+        createMapper().map(source, destination);
     }
 
     public <Source, Destination> Destination map(Source source, Class<Destination> destinationType) {
-        return new ModelMapper().map(source, destinationType);
+        return createMapper().map(source, destinationType);
     }
 
     public <SourceType, DestinationType>List<DestinationType> map(List<SourceType> source, Class<DestinationType> destinationType) {
-        ModelMapper modelMapper = new ModelMapper();
+        ModelMapper modelMapper =  createMapper();
         return source.stream()
                 .map(element -> modelMapper.map(element, destinationType))
                 .collect(Collectors.toList());
+    }
+
+    private ModelMapper createMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setAmbiguityIgnored(true);
+        return modelMapper;
     }
 
 }
